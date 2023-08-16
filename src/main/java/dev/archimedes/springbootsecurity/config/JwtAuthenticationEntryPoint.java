aaad15678058de46cde2fe17d3a05c061965bb80
ayu-sh-kr <akjaiswal2003@gmail.com>
2023-08-16 06:01:40 +0530

    Bug Resolved

--- src/main/java/dev/archimedes/springbootsecurity/config/JwtAuthenticationEntryPoint.java
+++ src/main/java/dev/archimedes/springbootsecurity/config/JwtAuthenticationEntryPoint.java
@@ -3,6 +3,7 @@ package dev.archimedes.springbootsecurity.config;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
@@ -11,11 +12,19 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 @Component
+@Slf4j
 public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        PrintWriter writer = response.getWriter();
-        writer.println("Access Denied !!" + authException.getMessage());
+        log.info(request.getRequestURL().toString());
+        if(request.getRequestURL().toString().equals("http://localhost:8080/index.html") || request.getRequestURL().toString().equals("http://localhost:8080/auth")){
+            response.setStatus(HttpServletResponse.SC_ACCEPTED);
+        }else{
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            PrintWriter writer = response.getWriter();
+            writer.println("Access Denied !!" + authException.getMessage());
+        }
+
     }
 }
--- src/main/java/dev/archimedes/springbootsecurity/config/SecurityConfig.java
+++ src/main/java/dev/archimedes/springbootsecurity/config/SecurityConfig.java
@@ -3,13 +3,14 @@ package dev.archimedes.springbootsecurity.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
 
@@ -23,16 +24,15 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/","/auth","/auth/**").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/user").hasRole("USER")
-                        .requestMatchers("/auth/**").permitAll()
                         .anyRequest().authenticated());
 
-        http.exceptionHandling(ex -> ex.authenticationEntryPoint(point))
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.exceptionHandling(ex -> ex.authenticationEntryPoint(point));
+        http .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         http.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
-        http.httpBasic(Customizer.withDefaults());
         return http.build();
     }
 
